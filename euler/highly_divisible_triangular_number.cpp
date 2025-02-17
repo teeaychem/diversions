2b6ca35c46b6153de62a335940c97046b2e9e054
teeaychem <ben@sparkes.uk.net>
2025-02-17 16:35:19 +0000

    A few more diversions
    Triangular numbers was something of a surprise.
    
    I have a feeling there's something to the observation these can be expressed as (n(n + 1))/2.
    Though, I'm not sure what this is.
    
    After a little too much thinking, a simple brute force out of interest for how difficult, and it's very not difficult.

---
+++ euler/highly_divisible_triangular_number.cpp
@@ -0,0 +1,42 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int64_t factors(int64_t n) {
+  int64_t count = 0;
+  for (int64_t i = 1; i <= sqrt(n); i++) {
+    if (n % i == 0) {
+
+      count++;
+      if (n / i != i) {
+
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+
+int main(int argc, char **argv) {
+
+  int64_t i = 1;
+  int64_t triangle = 1;
+  int64_t f = 0;
+
+  while (true) {
+    f = factors(triangle);
+
+    if (f < 500) {
+      i += 1;
+      triangle += i;
+
+    } else {
+      break;
+    }
+  }
+
+  cout << triangle << " " << f << endl;
+
+  return 0;
+}
