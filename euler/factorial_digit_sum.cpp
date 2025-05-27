f9e9eb9a5d5a8b141e6bf8828f41bce07ad7df83
teeaychem <bsparkes@stanford.edu>
2025-05-27 18:26:10 +0100

    factorial digit sum
    via the custom big int lib using karatsuba for multiplication

---
+++ euler/factorial_digit_sum.cpp
@@ -0,0 +1,23 @@
+#include <BigInt.hpp>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  BigInt::Int factorial = BigInt::Int(1);
+
+  for (int64_t i = 2; i <= 100; ++i) {
+
+    factorial = factorial * BigInt::Int(i);
+  }
+
+  cout << factorial << "\n";
+
+  BigInt::Int sum(0);
+
+  for (auto it = factorial.begin(); it != factorial.end(); ++it) {
+    sum = sum + BigInt::Int(*it);
+  }
+
+  cout << sum << "\n";
+}
